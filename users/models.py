1eb559eb372adbe7bc40d264daec944aac91acf9
solar1um <mitya.klimenko123@gmail.com>
2021-08-12 02:24:34 +0600

    new res

--- users/models.py
+++ users/models.py
@@ -11,7 +11,7 @@ class Profile(AbstractUser):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+        if img.height > 1920 or img.width > 1080:
+            output_size = (1920, 1080)
             img.thumbnail(output_size)
             img.save(self.image.path)
